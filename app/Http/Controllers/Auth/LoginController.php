5986478a8f279718c253211707510c5541f7cf75
KirRog97 <kirrog97@yandex.ru>
2019-05-19 18:25:32 +0300

    Added message when user has been authenticated

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Http\Request;
 
 class LoginController extends Controller
 {
@@ -35,5 +37,19 @@ class LoginController extends Controller
     public function __construct()
     {
         $this->middleware('guest')->except('logout');
+    }
+
+      /**
+     * The user has been authenticated.
+     * 
+     * Extended: vendor\laravel\framework\src\Illuminate\Foundation\Auth\AuthenticatesUsers.php
+     * 
+     * @param  \Illuminate\Http\Request  $request
+     * @param  mixed  $user
+     * @return mixed
+     */
+    protected function authenticated(Request $request)
+    {
+       return $request->session()->flash('success', 'Добро пожаловать, '.Auth::user()->username);
     }
 }
