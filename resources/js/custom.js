80f9d724a34a3219322a36d5dfd439040afd9347
KirRog97 <kirrog97@yandex.ru>
2019-05-19 17:41:19 +0300

    Added new Bootstrap JS classes (dropdown, alert)

--- resources/js/custom.js
+++ resources/js/custom.js
@@ -1,7 +1,13 @@
 $("img").mousedown(function () {
     return false;
 });
-
 $(function () {
     $('[data-toggle="tooltip"]').tooltip()
-})
+    $('.dropdown-toggle').dropdown();
+    $('.alert')
+        .alert('dispose')
+        .delay($(this).text().length * 2)
+        .slideUp(800, function () {
+            $(this).remove();
+        });
+});
