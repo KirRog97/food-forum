8cf59df363b172748e879da2050d75952edd1eab
KirRog97 <kirrog97@yandex.ru>
2019-06-19 13:24:27 +0300

    Solved DRY problem

--- app/Http/Controllers/RecipeFilter.php
+++ app/Http/Controllers/RecipeFilter.php
@@ -2,68 +2,40 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Dish;
 use App\Category;
 use App\Kitchen;
 use App\Ingredient;
 use Illuminate\Support\Facades\Cache;
-// use Illuminate\Support\Facades\DB;
 
 class RecipeFilter extends Controller
 {
-    public function getData()
+    public function cacheCheck($list, $class)
     {
-        if (Cache::has('ingredient_list')) {
-            $ingredient_list = Cache::get('ingredient_list');
+        if (Cache::has($list)) {
+            return  $el_list = Cache::get($list);
         } else {
-            Cache::rememberForever('ingredient_list', function () {
-                return Ingredient::all();
-            });
-            $ingredient_list = Cache::get('ingredient_list');
-        }
-
-        if (Cache::has('kitchen_list')) {
-            $kitchen_list = Cache::get('kitchen_list');
-        } else {
-            Cache::rememberForever('kitchen_list', function () {
-                return Kitchen::all();
-            });
-            $kitchen_list = Cache::get('kitchen_list');
-        }
-
-        if (Cache::has('category_list')) {
-            $category_list = Cache::get('category_list');
-        } else {
-            Cache::rememberForever('category_list', function () {
-                return Category::all();
-            });
-
-            $category_list = Cache::get('category_list');
-        }
-
-        return $urlData = [
-            'category_list' =>  $category_list, 'kitchen_list' =>  $kitchen_list,
-            'ingredient_list' =>  $ingredient_list
-        ];
+            $this->cacheRemember($list, $class);
+            return  $el_list = Cache::get($list);
+        };
     }
 
-    // public function outputData($ingredient_list, $kitchen_list, $category_list)
-    // {
-    //     $urlData = [
-    //         $ingredient_list, $kitchen_list, $category_list
-    //     ];
+    public function cacheRemember($list, $class)
+    {
+        $arrToRemember = $class::all()->pluck('name');
+        Cache::put($list, $arrToRemember, now()->addMinutes(30));
+    }
 
-    //     return dd(json_encode($urlData));
-    // }
+    public function getData()
+    {
+        $ingredient_list =      $this->cacheCheck('Ingredient', Ingredient::class);
+        $kitchen_list =         $this->cacheCheck('Kitchen', Kitchen::class);
+        $dishes_list =          $this->cacheCheck('Dishes', Dish::class);
+        $category_list =        $this->cacheCheck('Category', Category::class);
 
-    // public function checkCache(String $string)
-    // {
-    //     if (Cache::has($string)) {
-    //         return $string = Cache::get($string);
-    //     } else {
-    //         Cache::rememberForever($string, function () {
-    //             return DB::table($tableName)->get();
-    //         });
-    //     }
-    // }
+        return response()->json([
+            'ingredient_list' =>  $ingredient_list, 'kitchen_list' =>  $kitchen_list,
+            'dishes_list' =>  $dishes_list, 'category_list' =>  $category_list
+        ]);
+    }
 }
