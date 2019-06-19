5fe326502a3fa8fa74bcb679d1c946e0081b1f6c
KirRog97 <kirrog97@yandex.ru>
2019-06-19 13:11:31 +0300

    Fixed mistakes (spelling, cascade deleting)

--- database/migrations/2019_05_13_103003_create_posts_table.php
+++ database/migrations/2019_05_13_103003_create_posts_table.php
@@ -11,18 +11,18 @@ class CreatePostsTable extends Migration
         Schema::create('posts', function (Blueprint $table) {
             $table->increments('id');
             $table->string('title');
-            $table->boolean('is_banned');
-            $table->boolean('is_muted');
             $table->unsignedInteger('user_id');
             $table->unsignedInteger('picture_id');
             $table->unsignedInteger('category_id');
             $table->unsignedInteger('kitchen_id');
             $table->unsignedInteger('dish_id');
             $table->text('instruction');
-            $table->text('desciption');
+            $table->text('description');
             $table->integer('TTC');
             $table->integer('COP');
             $table->integer('Kcal');
+            $table->boolean('is_banned')->default(0);
+            $table->boolean('is_muted')->default(0);
             $table->timestamps();
 
             $table->foreign('user_id')->references('id')->on('users');
@@ -30,14 +30,11 @@ class CreatePostsTable extends Migration
             $table->foreign('picture_id')->references('id')->on('pictures')
                 ->onUpdate('cascade')->onDelete('cascade');
 
-            $table->foreign('category_id')->references('id')->on('categories')
-                ->onUpdate('cascade')->onDelete('cascade');
+            $table->foreign('category_id')->references('id')->on('categories');
 
-            $table->foreign('kitchen_id')->references('id')->on('kitchens')
-                ->onUpdate('cascade')->onDelete('cascade');
+            $table->foreign('kitchen_id')->references('id')->on('kitchens');
 
-            $table->foreign('dish_id')->references('id')->on('dishes')
-                ->onUpdate('cascade')->onDelete('cascade');
+            $table->foreign('dish_id')->references('id')->on('dishes');
         });
     }
 
