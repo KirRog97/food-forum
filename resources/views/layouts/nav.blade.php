5bcfe7a5ec92b198a652cdfe45324c93c762434e
KirRog97 <kirrog97@yandex.ru>
2019-06-19 13:06:56 +0300

    Reworked Navbar(dropdown-menu, adaptive markup)

--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -1,94 +1,113 @@
-<nav>
-    <div class="search justify-content-start">
-        <form id="search-form" action="" method="post">
-            {{ csrf_field() }}
-            <button type="submit" id="search-btn" class="nav-icon">
-                <i class="fa fa-search" aria-hidden="true"></i>
-            </button>
-            <input type="search" autofocus name="search" id="search-field">
-        </form>
-    </div>
-
-
-    <ul class="nav navbar-ul flex-grow-0">
-        @guest
-
-        <li class="ml-2">
-            <a href="{{ route('login') }}" class="nav-link nav-icon m-auto"
-            data-toggle="tooltip" data-placement="bottom" title="Log In">
-                <i class="fas fa-sign-in-alt"></i>
-            </a>
-        </li>
-        <li class="ml-4 mr-2">
-            <a href="{{ route('register') }}" class="nav-link nav-icon m-auto"
-            data-toggle="tooltip" data-placement="bottom" title="Check In">
-                <i class="fas fa-address-card"></i>
-            </a>
-        </li>
-
-        @else
-
-        <li class="notification mx-2">
-            <div class="dropdown">
-                <button class="btn btn rounded-right shadow-none" type="button" id="dropdownMenu1" data-toggle="tooltip" data-placement="bottom" title="" data-original-title="Notification">
-                    <i class="fa fa-bell nav-icon" aria-hidden="true"></i>
+<div class="container-fluid px-0 py-3 fa-lg">
+    <div class="row justify-content-between align-items-center">
+        <div class="col-9 col-md-10">
+            <form class="form-inline" action="" method=" POST">
+                {{ csrf_field() }}
+                <button class="btn px-2 py-0 mr-2" type="submit">
+                    <i class="fa fa-search fa-lg" aria-hidden="true"></i>
                 </button>
-                <div class="dropdown-menu" aria-labelledby="dropdownMenu1">
-                    <a class="dropdown-header bg-dark text-white py-3" href="#">
-                        <span>{{ Auth::user()->name }}</span>
-                    </a>
-                    <a class="dropdown-item" href="#!">Профиль</a>
-                    <a class="dropdown-item" href="#!">Another action</a>
-                </div>
+                <input class="form-control form-control-sm flex-fill" type="search" name="search">
+            </form>
+        </div>
 
-                <div class="dropdown-menu p-0 mr-5" aria-labelledby="dropdownMenu1">
-                    <div class="card">
-                        <div class="card-header">
-                            {{__('Notification')}}
-                        </div>
-                        <div class="card-body p-0">
-                            <div class="list-group">
-                                <a href="#" class="list-group-item list-group-item-action active">{{__('Notification')}}</a>
-                                <a href="#" class="list-group-item list-group-item-action">{{__('Notification')}}</a>
-                                <a href="#" class="list-group-item list-group-item-action disabled">{{__('Notification')}}</a>
+        <div class="col-3 col-md-2">
+            <ul class="d-flex flex-row flex-nowrap justify-content-around">
+
+                @auth
+                <li>
+                    <div class="dropdown">
+                        <button class="btn px-2 py-0 m-auto shadow-none" type="button" data-toggle="dropdown"
+                            data-offset="-120px" aria-haspopup="true" aria-expanded="false">
+                            <i class="fa fa-bell fa-lg" aria-hidden="true"></i>
+                        </button>
+
+                        <div class="dropdown-menu text-center p-0 mr-5" style=" width: 250px;">
+                            <div class="card bg-dark border-dark">
+                                <div
+                                    class="card-header d-flex justify-content-between align-items-center text-white border-dark">
+                                    <span>{{__('Уведомления')}}</span>
+                                    <a href="#">
+                                        <i class="fas fa-cog p-1"></i>
+                                    </a>
+                                </div>
+                                <div class="card-body p-0">
+                                    <div class="list-group">
+                                        <a href="#"
+                                            class="list-group-item list-group-item-primary list-group-item-action rounded-0">
+                                            <span>{{__('Уведомление')}}</span>
+                                        </a>
+                                        <a href="#"
+                                            class="list-group-item list-group-item-primary list-group-item-action rounded-0">
+                                            <span>{{__('Уведомление')}}</span>
+                                        </a>
+                                        <a href="#"
+                                            class="list-group-item list-group-item-primary list-group-item-action rounded-0">
+                                            <span>{{__('Уведомление')}}</span>
+                                        </a>
+                                    </div>
+                                </div>
                             </div>
                         </div>
-                        <div class="card-footer text-muted">
-                                {{__('Footer')}}
+
+                    </div>
+                </li>
+
+                <li>
+                    <div class="dropdown">
+                        <button type="button" class="btn px-2 py-0 m-auto shadow-none" data-toggle="dropdown"
+                            data-offset="-120px" aria-haspopup="true" aria-expanded="false">
+                            <i class="fa fa-user-circle fa-lg" aria-hidden="true"></i>
+                        </button>
+                        <div class="dropdown-menu rounded border-dark text-center p-0">
+                            <a class="dropdown-header bg-dark text-white py-2" href="#">
+                                <img src="{{ Auth::user()->avatar->path }}" style="width:64px;" alt="User Avatar">
+                                <h5 class="mt-1">{{ Auth::user()->username }}</h5>
+                            </a>
+                            <a class="dropdown-item py-3" href="/users/{{ Auth::user()->id }}">
+                                {{__('Мой профиль')}}
+                            </a>
+                            <a class="dropdown-item py-3" href="#">
+                                {{__('Мои рецепты')}}
+                            </a>
+                            <a class="dropdown-item py-3" href="#">
+                                {{__('Понравилось')}}
+                            </a>
+                            <a class="dropdown-item py-3" href="#">
+                                {{__('Закладки')}}
+                            </a>
+
+                            <div class="dropdown-divider m-0"></div>
+
+                            <a class="dropdown-item  bg-dark text-white" href="{{ route('logout') }}"
+                                onclick="event.preventDefault(); $('#logout-form').submit();">
+                                <i class="fas fa-sign-out-alt py-2 mr-1"></i>
+                                <span>{{__('Выход')}}</span>
+                            </a>
+
+                            <form id="logout-form" action="{{ route('logout') }}" method="POST" hidden>
+                                {{ csrf_field() }}
+                            </form>
                         </div>
                     </div>
-                </div>
+                </li>
 
-            </div>
-        </li>
+                @else
 
-        <li class="nav-item dropdown mx-2">
-            <div class="btn-group dropleft">
-                <button type="button" class="btn rounded-right shadow-none" data-toggle="tooltip" data-placement="bottom" title="" data-original-title="Просмотреть профиль">
-                    <i class="fa fa-user-circle nav-icon" aria-hidden="true"></i>
-                </button>
-                <div class="dropdown-menu border-dark text-center p-0" x-placement="right-start" style="position: absolute; transform: translate3d(111px, 0px, 0px); top: 0px; left: 0px; will-change: transform; width: 200px;">
-
-                    <a class="dropdown-header bg-dark text-white py-2" href="#">
-                                <h5 class="mt-1">{{ Auth::user()->username }}</h5>
-
-                <a class="dropdown-item py-2" href="/users/{{ Auth::user()->id }}">{{__('Мой профиль')}}</a>
-                    <a class="dropdown-item py-2" href="#">{{__('Мои рецепты')}}</a>
-                    <a class="dropdown-item py-2" href="#">{{__('Понравилось')}}</a>
-                    <a class="dropdown-item py-2" href="#">{{__('Закладки')}}</a>
-
-                    <div class="dropdown-divider m-0"></div>
-                    <a class="dropdown-item  bg-dark text-white" href="{{ Auth::logout() }}">
-                        <i class="fas fa-sign-out-alt py-2 mr-1"></i>
-                        {{__('Log Out')}}
+                <li>
+                    <a href="{{ route('login') }}" class="px-2 py-0 m-auto" data-toggle="tooltip"
+                        data-placement="bottom" title="Вход">
+                        <i class="fas fa-sign-in-alt"></i>
                     </a>
-                </div>
-            </div>
+                </li>
+                <li>
+                    <a href="{{ route('register') }}" class="px-2 py-0 m-auto" data-toggle="tooltip"
+                        data-placement="bottom" title="Зарегистрироваться">
+                        <i class="fas fa-address-card"></i>
+                    </a>
+                </li>
 
-            <!-- https://appdividend.com/2018/06/02/laravel-avatar-image-upload-tutorial-with-example/ -->
-        </li>
-
-        @endguest
-    </ul>
-
-</nav>
+                @endauth
+            </ul>
+        </div>
+    </div>
+</div>
