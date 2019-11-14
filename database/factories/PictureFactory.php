8f35f85b117a46057aa5cd2c9b0dc05bfcde5d93
KirRog97 <45996269+KirRog97@users.noreply.github.com>
2019-11-14 15:30:56 +0300

    Location of pics changed to local storage

--- database/factories/PictureFactory.php
+++ database/factories/PictureFactory.php
@@ -4,7 +4,7 @@ use Faker\Generator as Faker;
 
 $factory->define(App\Picture::class, function (Faker $faker) {
     return [
-        'path'  =>  $faker->imageUrl(800, 600, 'food'),
+        'path'  =>  '/storage/' . $faker->image(public_path('storage'), 800, 600, 'food', false),
         'mime'  =>  'image/jpg',
         'size'  =>  $faker->randomDigitNotNull
     ];
@@ -12,7 +12,7 @@ $factory->define(App\Picture::class, function (Faker $faker) {
 
 $factory->defineAs(App\Picture::class, 'avatar', function (Faker $faker) {
     return [
-        'path'  =>  $faker->imageUrl(200, 200, 'people', true, 'Faker'),
+        'path'  =>  '/storage/' . $faker->image(public_path('storage'), 200, 200, 'people', false, 'Faker'),
         'mime'  =>  'image/jpg',
         'size'  =>  $faker->randomDigitNotNull
     ];
