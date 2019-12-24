da624baf3739d6015fa3a6ac9b3aa1cbc775fa50
KirRog97 <45996269+KirRog97@users.noreply.github.com>
2019-12-24 14:21:46 +0300

    Created factory for PostIngredient model

--- database/factories/PostIngredientFactory.php
+++ database/factories/PostIngredientFactory.php
@@ -4,6 +4,8 @@ use Faker\Generator as Faker;
 
 $factory->define(\App\PostIngredient::class, function (Faker $faker) {
     return [
-        //
+        'post_id'   =>   $faker->numberBetween(1, 20),
+        'user_id'   =>   $faker->numberBetween(1, 20),
+        'amount'    =>   $faker->randomNumber(3),
     ];
 });
