ac697ef1da83d5429bfbded4f024031f964ca863
KirRog97 <45996269+KirRog97@users.noreply.github.com>
2020-01-18 16:28:04 +0300

    FIx classes. Added href attribute for <a>

--- resources/views/components/social_buttons.blade.php
+++ resources/views/components/social_buttons.blade.php
@@ -1,16 +1,24 @@
 <div class="container-fluid">
     <div class="row justify-content-around text-center">
-        <div class="col=auto">
-            <a href="" class="social_icon"><i class="fab fa-vk"></i></a>
+        <div class="col-auto">
+            <a href="https://vk.com/eda_ru" class="social_icon">
+                <i class="fab fa-vk"></i>
+            </a>
         </div>
-        <div class="col=auto">
-            <a href="" class="social_icon"><i class="fab fa-facebook-f"></i></a>
+        <div class="col-auto">
+            <a href="https://www.facebook.com/AfishaEda?fref=ts" class="social_icon">
+                <i class="fab fa-facebook-f"></i>
+            </a>
         </div>
-        <div class="col=auto">
-            <a href="" class="social_icon"><i class="fab fa-instagram"></i></a>
+        <div class="col-auto">
+            <a href="https://www.instagram.com/edaruofficial/" class="social_icon">
+                <i class="fab fa-instagram"></i>
+            </a>
         </div>
-        <div class="col=auto">
-            <a href="" class="social_icon"><i class="fab fa-google-plus-g"></i></a>
+        <div class="col-auto">
+            <a href="https://twitter.com/afisha_eda" class="social_icon">
+                <i class="fab fa-twitter"></i>
+            </a>
         </div>
     </div>
 </div>
