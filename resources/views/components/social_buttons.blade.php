4482e5a852ec21456f58cc62f8d51a265ab2ab99
KirRog97 <45996269+KirRog97@users.noreply.github.com>
2020-03-11 19:22:31 +0300

    FIxed social_icon class position

--- resources/views/components/social_buttons.blade.php
+++ resources/views/components/social_buttons.blade.php
@@ -1,22 +1,22 @@
 <div class="container-fluid">
     <div class="row justify-content-around text-center">
-        <div class="col-auto">
-            <a href="https://vk.com/eda_ru" class="social_icon">
+        <div class="col-auto social_icon">
+            <a href="https://vk.com/eda_ru">
                 <i class="fab fa-vk"></i>
             </a>
         </div>
-        <div class="col-auto">
-            <a href="https://www.facebook.com/AfishaEda?fref=ts" class="social_icon">
+        <div class="col-auto social_icon">
+            <a href="https://www.facebook.com/AfishaEda?fref=ts">
                 <i class="fab fa-facebook-f"></i>
             </a>
         </div>
-        <div class="col-auto">
-            <a href="https://www.instagram.com/edaruofficial/" class="social_icon">
+        <div class="col-auto social_icon">
+            <a href="https://www.instagram.com/edaruofficial/">
                 <i class="fab fa-instagram"></i>
             </a>
         </div>
-        <div class="col-auto">
-            <a href="https://twitter.com/afisha_eda" class="social_icon">
+        <div class="col-auto social_icon">
+            <a href="https://twitter.com/afisha_eda">
                 <i class="fab fa-twitter"></i>
             </a>
         </div>
