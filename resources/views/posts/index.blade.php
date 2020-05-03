fc1222dd1433abddb626141da2f78d04ae2780af
KirRog97 <45996269+KirRog97@users.noreply.github.com>
2020-05-03 18:06:56 +0300

    Added @json Blade directive for vue variable

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -9,48 +9,30 @@
 
 <div class="container-fluid">
 
-    <x-heading>
-        Книга рецептов
-    </x-heading>
+  <x-heading>
+    Книга рецептов
+  </x-heading>
 
+  <div class="position-relative text-center text-muted text-justify mb-5">
+    <p>
+      Ищите рецепты, выбирая категорию блюда, его подкатегорию, кухню или меню.
+    </p>
+  </div>
 
-    <div class="position-relative text-center text-muted text-justify mb-5">
-        <p>
-            Ищите рецепты, выбирая категорию блюда, его подкатегорию, кухню или меню.
-        </p>
-    </div>
+  <div class="recipe-list">
 
-    <div class="recipe-list">
+    @foreach ($posts as $post)
+    <post-index-card userId="{{$post->user->id}}" username="{{$post->user->username}}"
+      userAvatarPath="{{$post->user->avatar->path}}" postId="{{$post->id}}" postPicturePath="{{$post->pictures->path}}"
+      postTitle="{{$post->title}}" postTTC="{{$post->TTC}}" postCOP="{{$post->COP}}" postKcal="{{$post->Kcal}}"
+      :postIngredients='@json($post->ingredients)'>
+    </post-index-card>
+    @endforeach
+  </div>
 
-        @if (count($posts) > 0)
-
-        @foreach ($posts as $post)
-
-        <post-index-card
-        userId="{{$post->user->id}}"
-        username="{{$post->user->username}}"
-        userAvatarPath="{{$post->user->avatar->path}}"
-        postId="{{$post->id}}"
-        postPicturePath="{{$post->pictures->path}}"
-        postTitle="{{$post->title}}"
-        postTTC="{{$post->TTC}}"
-        postCOP="{{$post->COP}}"
-        postKcal="{{$post->Kcal}}"
-        >
-        </post-index-card>
-
-        @endforeach
-    </div>
-
-    <div class="d-flex justify-content-center mt-3">
-        {{ $posts->links() }}
-    </div>
-
-    @else
-    <div class="alert-danger rounded-pill text-center p-1 m-2 border border-danger text-uppercase">
-        <h1>Эта книга пока что пуста... Пока что...</h1>
-    </div>
-    @endif
+  <div class="d-flex justify-content-center mt-3">
+    {{ $posts->links() }}
+  </div>
 
 </div>
 @endsection
