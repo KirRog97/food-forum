5a1946878b3845743d5c0753966be660510feff0
KirRog97 <kirrog97@yandex.ru>
2020-07-17 13:34:31 +0300

    Updated way to plug in CSS and JS. Added favicon.

--- resources/views/layouts/html-head.blade.php
+++ resources/views/layouts/html-head.blade.php
@@ -3,6 +3,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <meta name="csrf-token" content="{{ csrf_token() }}">
   <title>@yield('page_title')</title>
-  <script src="{{ asset('js/app.js') }}" defer></script>
-  <link href="{{ asset('css/app.min.css') }}" rel="stylesheet" type="text/css" />
+  <link rel="icon" href="{{ URL::asset('/favicon.png') }}" type="image/x-icon" />
+  <link rel="stylesheet" type="text/css" href="{{ URL::asset('/css/app.css')  }}" defer>
+  <script src="{{ URL::asset('/js/app.js')  }}" defer></script>
 </head>
