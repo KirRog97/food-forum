128d07ea1e3ba02789035c8e23cbc6f8c9dfb08e
KirRog97 <kirrog97@yandex.ru>
2020-08-11 16:48:16 +0300

    Switching to Tailwind. Remake footer styles&view

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,16 +1,33 @@
-<div class="container-fluid">
-    <div class="row justify-content-center align-content-lg-center">
-        <div class="col-12 col-md-9 d-flex flex-wrap justify-content-md-start justify-content-between py-1 px-3">
-            <a class="col-4 col-md-auto footer-menu-link py-1 py-lg-0 px-2" href="/">Главная</a>
-            <a class="col-4 col-md-auto footer-menu-link py-1 py-lg-0 px-2" href="/posts">Рецепты</a>
-            <a class="col-4 col-md-auto footer-menu-link py-1 py-lg-0 px-2" href="">Идеи</a>
-            <a class="col-4 col-md-auto footer-menu-link py-1 py-lg-0 px-2" href="/users">Авторы</a>
-            <a class="col-4 col-md-auto footer-menu-link py-1 py-lg-0 px-2" href="">Журналы</a>
-            <a class="col-4 col-md-auto footer-menu-link py-1 py-lg-0 px-2" href="">Реклама</a>
-            <a class="col-4 col-md-auto footer-menu-link py-1 py-lg-0 px-2" href="">FAQ</a>
+<div class="flex flex-auto flex-wrap justify-center py-4 md:py-4 mx-auto">
+    <div class="footer__link-wrapper">
+        <a class="footer__link" href="/">Главная</a>
+        <a class="footer__link" href="/posts">Рецепты</a>
+        <a class="footer__link" href="">Идеи</a>
+        <a class="footer__link" href="/users">Авторы</a>
+        <a class="footer__link" href="">Журналы</a>
+        <a class="footer__link" href="">Реклама</a>
+        <a class="footer__link" href="">FAQ</a>
+    </div>
+    <div class="footer__icons">
+        <div class="footer__icons-item">
+            <a href="https://vk.com/eda_ru">
+                <i class="fab fa-vk fa-lg"></i>
+            </a>
         </div>
-        <div class="col-4 col-md-3 py-1 px-0">
-            <x-social_buttons></x-social_buttons>
+        <div class="footer__icons-item">
+            <a href="https://www.facebook.com/AfishaEda?fref=ts">
+                <i class="fab fa-facebook-f fa-lg"></i>
+            </a>
+        </div>
+        <div class="footer__icons-item">
+            <a href="https://www.instagram.com/edaruofficial/">
+                <i class="fab fa-instagram fa-lg"></i>
+            </a>
+        </div>
+        <div class="footer__icons-item">
+            <a href="https://twitter.com/afisha_eda">
+                <i class="fab fa-twitter text-base fa-lg"></i>
+            </a>
         </div>
     </div>
 </div>
