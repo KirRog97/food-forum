7b97e76f9d7e6dc769cdde3bf200ed92dbab5de5
KirRog97 <kirrog97@yandex.ru>
2020-08-11 15:44:52 +0300

    Switching to Tailwind. Remake heading

--- resources/views/components/heading.blade.php
+++ resources/views/components/heading.blade.php
@@ -1,8 +1,10 @@
-<div class="heading">
-    <h3>
-        {{ $slot }}
-    </h3>
-    <div class="heading-box">
-        <img src="/images/olive_branche.png" alt="">
+<div class="heading-container">
+    <div class="heading">
+        <div class="heading-line"></div>
+        <img class="heading-logo" src="/images/olive_branche.png" alt="Heading logo">
+        <div class="heading-line"></div>
     </div>
+    <span class="text-2xl sm:text-3xl font-display">
+        {{ $slot }}
+    </span>
 </div>
