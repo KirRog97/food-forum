1932b1e820b468c78c9cd3031bb77b6ad5aa66b7
KirRog97 <kirrog97@yandex.ru>
2020-08-11 16:21:24 +0300

    Switching to Tailwind. Rework post compilation

--- resources/views/layouts/compilation.blade.php
+++ resources/views/layouts/compilation.blade.php
@@ -1,127 +1,121 @@
-<post-selection></post-selection>
+<div class="post-compilation">
 
-<div class="post-compilation py-3">
-
-  <div class="post-compilation-item grid-item-1-1">
-    <img class="img-fluid-center position-absolute" src="/images/recipes/пельмени.jpg" alt="Изображение рецепта дня">
-    <div class="post-compilation-typography vignette-large justify-content-center align-content-center">
-      <div class="post-compilation-item-category">
-        <h5>
-          Рецепт дня
-        </h5>
-      </div>
-      <div class="post-compilation-item-large-title">
-        <h2>
-          Курины ножки на гриле
-        </h2>
-      </div>
-    </div>
-  </div>
-
-  <div class="post-compilation-item grid-item-1-2">
-    <img class="img-fluid-center position-absolute" src="/images/recipes/пельмени.jpg" alt="Изображение рецепта дня">
-    <div class="post-compilation-typography vignette-large justify-content-center align-content-center">
-      <div class="post-compilation-item-category">
-        <h5>
-          Лучшие рецепты недели
-        </h5>
-      </div>
-      <div class="post-compilation-item-large-title">
-        <h2>
-          7 лучших рецептов
-        </h2>
-      </div>
-    </div>
-  </div>
-
-  <div class="post-compilation-item grid-item-2-1">
-    <img class="img-fluid-center position-absolute" src="/images/spice.jpg" alt="">
-    <div class="post-compilation-typography vignette align-content-start p-2">
-      <div class="post-compilation-item-tags">
-        <div class="post-compilation-item-tag">
-          <i class="fas fa-drumstick-bite mr-1"></i>
-          <h6>Вкусно</h6>
+    <div class="post-compilation__item grid-item-1-1">
+        <img class="img-fluid--cover" src="/images/recipes/пельмени.jpg" alt="Изображение рецепта дня">
+        <div class="post-compilation__typography post-compilation__typography--large bg-vignette">
+            <span class="post-compilation__category">
+                Рецепт дня
+            </span>
+            <span class="post-compilation__title post-compilation__title--large">
+                Курины ножки на гриле
+            </span>
+            <div class="post-compilation__accessory">
+                <div class="post-compilation__accessory__line"></div>
+                <div class="post-compilation__accessory__logo">
+                    <i class="fas fa-hotdog fa-2x"></i>
+                </div>
+                <div class="post-compilation__accessory__line"></div>
+            </div>
         </div>
-      </div>
-      <div class="post-compilation-item-title p-1 m-0">
-        <h5>Как приготовить пирог в одной посуде 5 рецептов</h5>
-      </div>
     </div>
-  </div>
 
-  <div class="post-compilation-item grid-item-2-2">
-    <img class="img-fluid-center position-absolute" src="/images/spice.jpg" alt="">
-    <div class="post-compilation-typography vignette align-content-start p-2">
-      <div class="post-compilation-item-tags">
-        <div class="post-compilation-item-tag">
-          <i class="fas fa-drumstick-bite mr-1"></i>
-          <h6>Вкусно</h6>
+
+    <div class="post-compilation__item grid-item-1-2">
+        <img class="img-fluid--cover" src="/images/recipes/пельмени.jpg" alt="Изображение рецепта дня">
+        <div class="post-compilation__typography post-compilation__typography--large bg-vignette">
+            <span class="post-compilation__category">
+                Рецепт дня
+            </span>
+            <span class="post-compilation__title post-compilation__title--large">
+                Курины ножки на гриле
+            </span>
+            <div class="post-compilation__accessory">
+                <div class="post-compilation__accessory__line"></div>
+                <div class="post-compilation__accessory__logo">
+                    <i class="fas fa-hotdog fa-2x"></i>
+                </div>
+                <div class="post-compilation__accessory__line"></div>
+            </div>
         </div>
-      </div>
-      <div class="post-compilation-item-title p-1 m-0">
-        <h5>Как приготовить пирог в одной посуде 5 рецептов</h5>
-      </div>
     </div>
-  </div>
 
-  <div class="post-compilation-item grid-item-2-3">
-    <img class="img-fluid-center position-absolute" src="/images/spice.jpg" alt="">
-    <div class="post-compilation-typography vignette align-content-start p-2">
-      <div class="post-compilation-item-tags">
-        <div class="post-compilation-item-tag">
-          <i class="fas fa-drumstick-bite mr-1"></i>
-          <h6>Вкусно</h6>
-        </div>
-      </div>
-      <div class="post-compilation-item-title p-1 m-0">
-        <h5>Как приготовить пирог в одной посуде 5 рецептов</h5>
-      </div>
-    </div>
-  </div>
-
-  <div class="post-compilation-item grid-item-3-1">
-    <img class="img-fluid-center position-absolute" src="/images/spice.jpg" alt="">
-    <div class="post-compilation-typography vignette align-content-start p-2">
-      <div class="post-compilation-item-tags">
-        <div class="post-compilation-item-tag">
-          <i class="fas fa-drumstick-bite mr-1"></i>
-          <h6>Вкусно</h6>
-        </div>
-      </div>
-      <div class="post-compilation-item-title p-1 m-0">
-        <h5>Как приготовить пирог в одной посуде 5 рецептов</h5>
-      </div>
-    </div>
-  </div>
-
-  <div class="post-compilation-item grid-item-3-2">
-    <img class="img-fluid-center position-absolute" src="/images/spice.jpg" alt="">
-    <div class="post-compilation-typography vignette align-content-start p-2">
-      {{-- <div class="post-compilation-item-tags">
-                <div class="post-compilation-item-tag">
+    <div class="post-compilation__item grid-item-2-1">
+        <img class="img-fluid--cover absolute" src="/images/spice.jpg" alt="">
+        <div class="post-compilation__typography post-compilation__typography--base vignette">
+            <div class="post-compilation__tag-wrapper">
+                <div class="post-compilation__tag">
                     <i class="fas fa-drumstick-bite mr-1"></i>
                     <h6>Вкусно</h6>
                 </div>
-            </div> --}}
-      <div class="post-compilation-item-title p-1 m-0">
-        <h5>Как приготовить пирог в одной посуде 5 рецептов</h5>
-      </div>
-    </div>
-  </div>
-
-  <div class="post-compilation-item grid-item-3-3">
-    <img class="img-fluid-center position-absolute" src="/images/spice.jpg" alt="">
-    <div class="post-compilation-typography vignette align-content-start p-2">
-      <div class="post-compilation-item-tags">
-        <div class="post-compilation-item-tag">
-          <i class="fas fa-drumstick-bite mr-1"></i>
-          <h6>Вкусно</h6>
+            </div>
+            <span class="post-compilation__title post-compilation__title--base">
+                Как приготовить пирог в одной посуде 5 рецептов
+            </span>
         </div>
-      </div>
-      <div class="post-compilation-item-title p-1 m-0">
-        <h5>Как приготовить пирог в одной посуде 5 рецептов</h5>
-      </div>
     </div>
-  </div>
+
+    <div class="post-compilation__item grid-item-2-2">
+        <img class="img-fluid--cover absolute" src="/images/spice.jpg" alt="">
+        <div class="post-compilation__typography post-compilation__typography--base vignette">
+            <div class="post-compilation__tag-wrapper">
+                <div class="post-compilation__tag">
+                    <i class="fas fa-drumstick-bite mr-1"></i>
+                    <h6>Вкусно</h6>
+                </div>
+            </div>
+            <span class="post-compilation__title post-compilation__title--base">Как приготовить пирог в одной посуде 5
+                рецептов</span>
+        </div>
+    </div>
+
+    <div class="post-compilation__item grid-item-2-3">
+        <img class="img-fluid--cover absolute" src="/images/spice.jpg" alt="">
+        <div class="post-compilation__typography post-compilation__typography--base vignette">
+            <div class="post-compilation__tag-wrapper">
+                <div class="post-compilation__tag">
+                    <i class="fas fa-drumstick-bite mr-1"></i>
+                    <h6>Вкусно</h6>
+                </div>
+            </div>
+            <span class="post-compilation__title post-compilation__title--base">Как приготовить пирог в одной посуде 5
+                рецептов</span>
+        </div>
+    </div>
+
+    <div class="post-compilation__item grid-item-3-1">
+        <img class="img-fluid--cover absolute" src="/images/spice.jpg" alt="">
+        <div class="post-compilation__typography post-compilation__typography--base vignette">
+            <div class="post-compilation__tag-wrapper">
+                <div class="post-compilation__tag">
+                    <i class="fas fa-drumstick-bite mr-1"></i>
+                    <h6>Вкусно</h6>
+                </div>
+            </div>
+            <span class="post-compilation__title post-compilation__title--base">Как приготовить пирог в одной посуде 5
+                рецептов</span>
+        </div>
+    </div>
+
+    <div class="post-compilation__item grid-item-3-2">
+        <img class="img-fluid--cover absolute" src="/images/spice.jpg" alt="">
+        <div class="post-compilation__typography post-compilation__typography--base vignette">
+            <span class="post-compilation__title post-compilation__title--base">Как приготовить пирог в одной посуде 5
+                рецептов</span>
+        </div>
+    </div>
+
+    <div class="post-compilation__item grid-item-3-3">
+        <img class="img-fluid--cover absolute" src="/images/spice.jpg" alt="">
+        <div class="post-compilation__typography post-compilation__typography--base vignette">
+            <div class="post-compilation__tag-wrapper">
+                <div class="post-compilation__tag">
+                    <i class="fas fa-drumstick-bite mr-1"></i>
+                    <h6>Вкусно</h6>
+                </div>
+            </div>
+            <span class="post-compilation__title post-compilation__title--base">Как приготовить пирог в одной посуде 5
+                рецептов</span>
+        </div>
+    </div>
 
 </div>
