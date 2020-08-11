7d43cbbfc8447cae351cf44d0db86b22c5215cda
KirRog97 <kirrog97@yandex.ru>
2020-08-11 16:44:00 +0300

    Switching to Tailwind. Remake 'ingredients' views

--- resources/views/ingredients/index.blade.php
+++ resources/views/ingredients/index.blade.php
@@ -1,62 +1,89 @@
 @extends('layouts.app')
 
 @section('page_title')
-Книга ингредиентов
+Eда - Книга ингредиентов
 @endsection
 
 @section('content')
 
-<div class="container-fluid">
-
-    <x-heading>
-        Книга ингредиентов
-    </x-heading>
+<x-heading>
+    Книга ингредиентов
+</x-heading>
 
 <x-sub-heading>
     Здесь вы можете ознакомиться с ингредиентами, которые будут использоваться в ваших рецептах.
 </x-sub-heading>
 
-    @if (count($ingredients) > 0)
+@if (count($ingredients) > 0)
 
+<div class="space-y-8 md:space-y-6">
     @foreach ($ingredients as $ingredient)
 
-    <div class="row px-0 py-3">
-        <div class="col-4 p-0 rounded">
-            @if ($ingredient->picture->path)
-            <img class="img-fluid-center" src="{{ $ingredient->picture->path }}" alt="Ingredient Picture">
-            @else
-            <img class="img-fluid-center" src="/images/ingredients/default_ingredient_img.svg"
-                alt="Ingredient Default Picture">
-            @endif
+    <div class="grid grid-cols-6 gap-3 bg-secondary-100 rounded-lg p-2 md:p-4">
+        {{-- Ing Image --}}
+        <div class="col-span-3 row-span-1 md:row-span-3 flex justify-center items-center">
+            {{-- <img class="w-full object-cover overflow-hidden rounded" src="{{ $ingredient->picture->path }}"
+            alt="{{ $ingredient->name }} Picture"> --}}
+
+            <img class="w-full object-cover overflow-hidden rounded" src="/images/login_background.jpg"
+                alt="{{ $ingredient->name }} Picture">
         </div>
-        <div class="col rounded text-justify text-secondary px-3">
-            <a class="text-center font-weight-light py-2" href="/ingredients/{{ $ingredient->id }}">
-                <h3>
+
+        {{-- Ing Name --}}
+        <div class="col-span-3 row-span-1 flex justify-center items-center p-1">
+            <a class="text-3xl sm:text-4xl md:text-3xl text-center leading-none"
+                href="/ingredients/{{ $ingredient->id }}">
+                <span>
                     {{ $ingredient->name }}
-                </h3>
+                </span>
             </a>
-            <hr>
-            <p class="text-wrap px-3 m-auto">
-                @if ($ingredient->description)
+        </div>
+
+        {{-- Ing Description --}}
+        <div class="col-span-6 md:col-span-3 row-span-2 border-t-2 border-solid border-secondary-900 p-3">
+            {{--
+                Issue: 'whitespace-pre-line' creating whtie space in fist string of <p>.
+                Amount of white space equal to HTML space in here
+
+                Decision: make negative margin -mt-4
+
+                Right decision: make shorter 'class'
+                and palce 'blade variablble' close to tag "p"
+            --}}
+            {{-- https://service.webboss.pro/text-generator --}}
+            <p
+                class="text-lg text-secondary-900 leading-normal md:leading-snug font-normal whitespace-pre-line overflow-hidden font-display font-serif -mt-4">
                 {{ $ingredient->description }}
-                @else
-                Для данного ингредиента описание отсутствует
-                @endif
+                {{-- С другой стороны постоянное информационно-пропагандистское обеспечение нашей деятельности обеспечивает
+                широкому кругу (специалистов) участие в формировании позиций, занимаемых участниками в отношении
+                поставленных задач.
+
+                Равным образом рамки и место обучения кадров влечет за собой процесс внедрения и модернизации системы
+                обучения кадров,соответствует насущным потребностям.
+
+                С другой стороны рамки и место обучения кадров способствует подготовки и реализации модели развития.
+                Идейные соображения высшего порядка, а также рамки и место обучения кадров обеспечивает широкому кругу
+                (специалистов)
+                участие в формировании новых предложений.
+                Товарищи! сложившаяся структура организации представляет собой интересный эксперимент проверки
+                направлений прогрессивного развития. --}}
             </p>
         </div>
     </div>
 
     @endforeach
+</div>
 
 <x-paginate-links>
     {{$ingredients->links()}}
 </x-paginate-links>
 
-    @else
-    <div class="alert-danger rounded-pill text-center p-1 m-2 border border-danger text-uppercase">
-        <h1>Эта книга пока что пуста... Пока что...</h1>
-    </div>
-    @endif
+@else
 
-</div>
+<x-message-empty-collection>
+    Список ингредиентов временно пуст
+</x-message-empty-collection>
+
+@endif
+
 @endsection
--- resources/views/ingredients/show.blade.php
+++ resources/views/ingredients/show.blade.php
@@ -1,23 +1,23 @@
 @extends('layouts.app')
 
 @section('page_title')
-Рецепт
+Ингредиент "{{ $ingredient->name }}"
 @endsection
 
 @section('content')
 
-<div class="container-fluid">
-  <div class="row">
-    <div class="col p-0" style="background-color: lightgrey;">
-      <div class="card text-white border-0 position-relative">
-        <img class="card-img-top" src="{{ $ingredient->picture->path }}" alt="Ingredient image">
-      </div>
+<div class="flex flex-auto mx-auto">
+    <div class="row">
+        <div class="col p-0" style="background-color: lightgrey;">
+            <div class="card text-white border-0 relative">
+                <img class="card-img-top" src="{{ $ingredient->picture->path }}" alt="Ingredient image">
+            </div>
 
-      <div class="my-3">
-        <h2 class="post-about-heading">{{ $ingredient->name }}</h2>
-        <p class="post-about-paragraph">{{ $ingredient->description }}</p>
-      </div>
+            <div class="my-3">
+                <h2 class="post-about-heading">{{ $ingredient->name }}</h2>
+                <p class="post-about-paragraph">{{ $ingredient->description }}</p>
+            </div>
+        </div>
     </div>
-  </div>
 </div>
 @endsection
