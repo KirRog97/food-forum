2b9c0eb4f7b4dad1ae5691efc85d5dcd0284a222
KirRog97 <kirrog97@yandex.ru>
2020-09-14 19:27:48 +0300

    Change style of app's base layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,21 +6,21 @@
 <body>
     <div id="app">
         <el-container>
-            <el-aside width="64px">
+            <el-aside class="hidden md:block" width="64px">
                 <sidebar></sidebar>
                 <sidebar-drawer></sidebar-drawer>
             </el-aside>
             <el-container>
-                <el-header height="auto">
+                <el-header height="64px">
                     @include('layouts.nav')
                 </el-header>
-                <el-main style="min-height: 100vh;">
+                <el-main>
                     <vue-snotify></vue-snotify>
                     <backtop></backtop>
                     @include('components.message')
                     @yield('content')
                 </el-main>
-                <el-footer height="fit-content">
+                <el-footer class="bg-secondary-900" height="fit-content">
                     @include('layouts.footer')
                 </el-footer>
             </el-container>
