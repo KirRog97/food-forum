9caaad8b882e052c6f590ca02658dfe67bce35ea
KirRog97 <kirrog97@yandex.ru>
2020-11-14 17:51:03 +0300

    Cleaned all unneeded stuff

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,9 +1,3 @@
-/**
- * First we will load all of this project's JavaScript dependencies which
- * includes Vue and other libraries. It is a great starting point when
- * building robust, powerful web applications using Vue and Laravel.
- */
-
 require("./bootstrap");
 
 window.Vue = require("vue");
@@ -30,28 +24,9 @@ Vue.use(Snotify, {
   }
 });
 
-Vue.toArrayOfObjects = function(data) {
-  let resultArray = [];
-  _.each(data, function(value, key) {
-    resultArray.push({
-      id: key,
-      name: value
-    });
-  });
-  return resultArray;
-};
-
 require("./components/index");
-
-/**
- * Next, we will create a fresh Vue application instance and attach it to
- * the page. Then, you may begin adding components to this application
- * or customize the JavaScript scaffolding to fit your unique needs.
- */
 
 const app = new Vue({
   el: "#app",
   store
 });
-
-require("./custom");
