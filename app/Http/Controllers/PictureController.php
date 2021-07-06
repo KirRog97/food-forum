aa1e7500229f231caea56f053e3cf293a2c9aa40
KirRog97 <KirRog97@yandex,ru>
2021-07-06 17:53:03 +0300

    Add Update method to PictureController

--- app/Http/Controllers/PictureController.php
+++ app/Http/Controllers/PictureController.php
@@ -9,15 +9,6 @@ use Illuminate\Support\Facades\Storage;
 
 class PictureController extends Controller
 {
-
-    // public function __construct()
-    // {
-    //     $this->middleware('auth', ['except' => [
-    //         'index', 'show'
-    //     ]]);
-    // }
-
-
     /**
      * Store a newly created resource in storage.
      *
@@ -48,6 +39,53 @@ class PictureController extends Controller
         ]);
 
         $request->session()->put('post_picture', $picture->id);
+
+        return response()->json(
+            [
+                'id'    =>  $picture->id,
+                'path'  =>  $picture->path
+            ],
+            200
+        );
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Picture  $picture
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Picture $picture)
+    {
+        $validator = Validator::make($request->all(), [
+            'file' => [
+                'required',
+                'file',
+                'filled',
+                'image',
+            ],
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json(['message' => 'Sended file check faild'], 500);
+        }
+
+        try {
+            // delete "storage/" in string
+
+            Storage::delete($picture->path);
+            // $oldPicturePath = substr($picture->path, 8);
+            // Storage::disk('public')->delete($oldPicturePath);
+            $path = $request->file->store('uploads', 'public');
+            $picture->update([
+                'path'  =>  '/storage/' . $path,
+                'mime'  =>  $request->file->getMimeType(),
+                'size'  =>  $request->file->getSize()
+            ]);
+        } catch (\Throwable $exepction) {
+            return response()->json(['message' => $exepction->getMessage()], 500);
+        }
 
         return response()->json(
             [
