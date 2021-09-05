955f604bec0337fbcee9b8026d95ac8af7ab37d9
KirRog97 <KirRog97@yandex,ru>
2021-09-05 16:52:25 +0300

    Move Ingredient.show View to Tailwind

--- resources/views/ingredients/show.blade.php
+++ resources/views/ingredients/show.blade.php
@@ -6,17 +6,35 @@
 
 @section('content')
 
-<div class="flex flex-auto mx-auto">
-    <div class="row">
-        <div class="col p-0" style="background-color: lightgrey;">
-            <div class="card text-white border-0 relative">
-                <img class="card-img-top" src="{{ $ingredient->picture->path }}" alt="Ingredient image">
-            </div>
 
-            <div class="my-3">
-                <h2 class="post-about-heading">{{ $ingredient->name }}</h2>
-                <p class="post-about-paragraph">{{ $ingredient->description }}</p>
-            </div>
+<div class="flex flex-col mx-auto">
+    <div class="relative w-full bg-secondary-100">
+        {{-- Ingredient Image --}}
+        <div class="relative pb-2/3">
+            <img class="absolute h-full w-full object-cover" src="{{ $ingredient->picture->path}}" alt="Ingredient
+            image">
+
+            {{-- Placeholder --}}
+            {{-- <img class="absolute h-full w-full object-cover" src="/images/login_background.jpg" alt="Post image"> --}}
+        </div>
+
+
+    </div>
+
+    {{-- Ingredient main body--}}
+    <div class="flex flex-auto flex-wrap justify-center bg-secondary-100 px-6 py-8 mx-auto space-y-6 sm:space-y-8">
+        {{--Ingredient title and description--}}
+        <div class="flex justify-center flex-wrap">
+
+            {{--Ingredient title--}}
+            <span class="text-2xl sm:text-4xl text-primary-600 text-center leading-tight font-handwritten mb-1 sm:mb-2">
+                {{ $ingredient->name }}
+            </span>
+
+            {{--Ingredient description--}}
+            <p class="w-full text-base sm:text-lg text-secondary-800 leading-snug sm:leading-normal">
+                {{ $ingredient->description }}
+            </p>
         </div>
     </div>
 </div>
