2995b2ce2072bedbb084da5d5e435b20392b48eb
KirRog97 <KirRog97@yandex,ru>
2021-09-05 15:32:17 +0300

    Change padding in low-width screens,del waste code

--- resources/views/layouts/auth.blade.php
+++ resources/views/layouts/auth.blade.php
@@ -9,16 +9,16 @@
             @yield('background_image')
         </div>
         <div
-            class="w-full sm:w-8/12 lg:w-5/12 flex flex-wrap content-center bg-secondary-800 border-0 sm:border-l-2 border-primary-500 px-5 py-2">
-            <div class="w-full flex flex-auto justify-center mb-3">
+            class="w-full sm:w-8/12 lg:w-5/12 flex flex-wrap content-center justify-center bg-secondary-800 border-0 sm:border-l-2 border-primary-500 px-12 py-10 sm:px-5 sm:py-2">
+            <div class="w-full flex flex-auto justify-center mb-8">
                 <a class="text-6xl text-primary-500 hover:text-primary-400 text-center font-handwritten" href="/home"
                     data-toggle="tooltip" data-placement="bottom" title="На главную">
                     Еда
                 </a>
             </div>
-            <div class="w-full">
-                @yield('form')
-            </div>
+
+            @yield('form')
+
         </div>
     </div>
 </body>
