a34892a01842abf70bcd29849984a6fae00dcc37
KirRog97 <KirRog97@yandex,ru>
2022-02-23 21:11:44 +0300

    Upgrade to ElementPlus (v2) from ElementUI (v1)

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,6 +4,10 @@ import { createApp, h } from "vue";
 import { createInertiaApp } from "@inertiajs/inertia-vue3";
 import { InertiaProgress } from "@inertiajs/progress";
 
+import "../sass/element-ui/element-variables.scss";
+import ElementPlus from "element-plus";
+import ElementPlusRussianLocale from "element-plus/lib/locale/lang/ru";
+
 const appName =
   window.document.getElementsByTagName("title")[0]?.innerText || "Laravel";
 
@@ -12,6 +16,9 @@ createInertiaApp({
   resolve: (name) => require(`./Pages/${name}.vue`),
   setup({ el, app, props, plugin }) {
     return createApp({ render: () => h(app, props) })
+      .use(ElementPlus, {
+        locale: ElementPlusRussianLocale,
+      })
       .use(plugin)
       .mixin({ methods: { route } })
       .mount(el);
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,31 @@
 const path = require("path");
 
+// ElementUI_Plus
+const AutoImport = require("unplugin-auto-import/webpack");
+const Components = require("unplugin-vue-components/webpack");
+const { ElementPlusResolver } = require("unplugin-vue-components/resolvers");
+
 module.exports = {
   resolve: {
     alias: {
       "@": path.resolve("resources/js"),
     },
+    extensions: [".ts", ".js", ".mjs", ".json"],
   },
+  module: {
+    rules: [
+      {
+        test: /\.mjs$/i,
+        resolve: { byDependency: { esm: { fullySpecified: false } } },
+      },
+    ],
+  },
+  plugins: [
+    AutoImport({
+      resolvers: [ElementPlusResolver()],
+    }),
+    Components({
+      resolvers: [ElementPlusResolver()],
+    }),
+  ],
 };
