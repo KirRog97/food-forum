72e7adc1b11a6325d7dabdfce1128e7eacd97a0b
KirRog97 <KirRog97@yandex,ru>
2022-02-23 20:46:36 +0300

    Update Routes to Laravel 8 standards

--- routes/api.php
+++ routes/api.php
@@ -2,21 +2,31 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\CategoryController;
+use App\Http\Controllers\Api\DishController;
+use App\Http\Controllers\Api\IngredientController;
+use App\Http\Controllers\Api\IngredientPostController;
+use App\Http\Controllers\Api\KitchenController;
+use App\Http\Controllers\Api\MenuController;
+use App\Http\Controllers\Api\PostController;
 
-// Route::middleware('auth:api')->get('/user', function (Request $request) {
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 //     return $request->user();
 // });
 
-Route::get('categories', 'Api\\CategoryController@index');
-Route::get('ingredients', 'Api\\IngredientController@index');
-Route::get('dishes', 'Api\\DishController@index');
-Route::get('kitchens', 'Api\\KitchenController@index');
-Route::get('menus', 'Api\\MenuController@index');
+Route::get('categories', [CategoryController::class, 'index']);
+Route::get('ingredients', [IngredientController::class, 'index']);
+Route::get('dishes', [DishController::class, 'index']);
+Route::get('kitchens', [KitchenController::class, 'index']);
+Route::get('menus', [MenuController::class, 'index']);
 
 
-Route::group(['prefix' => 'posts'], function () {
-    Route::get('/{post}/ingredients', 'Api\\IngredientPostController@index');
-    Route::get('/new', 'Api\\PostController@new');
-    Route::get('favorites/editor-choice', 'Api\\PostController@editorChoise');
-    Route::get('/favorites/top', 'Api\\PostController@top');
-});
+Route::group(
+    ['prefix' => 'posts'],
+    function () {
+        Route::get('/{post}/ingredients', [IngredientPostController::class, 'index']);
+        Route::get('favorites/editor-choice', [PostController::class, 'editorChoise']);
+        Route::get('/favorites/top', [PostController::class, 'top']);
+        Route::get('/new', [PostController::class, 'new']);
+    }
+);
--- routes/web.php
+++ routes/web.php
@@ -1,62 +1,78 @@
 <?php
 
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\IngredientController;
+use App\Http\Controllers\PostController;
+use App\Http\Controllers\DishController;
+use App\Http\Controllers\IngredientPostController;
+use App\Http\Controllers\PictureController;
+use App\Http\Controllers\LikeController;
 use Illuminate\Support\Facades\Route;
-use Illuminate\Support\Facades\Auth;
+use Inertia\Inertia;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+Route::redirect('/', '/home');
 
+Route::get(
+    '/home',
+    function () {
+        return Inertia::render('Home');
+    }
+)->name('home');
 
-Route::get('home', 'HomeController@index')->name('home');
+Route::resource('users', UserController::class);
+Route::resource('users', UserController::class)
+    ->except(
+        [
+            'index', 'show'
+        ]
+    )->middleware(['auth', 'own.user']);
 
-Route::resource('users', 'UserController');
-Route::resource('users', 'UserController')->except([
-    'index', 'show'
-])->middleware(['auth', 'own.user']);
+Route::get('/users/{user}/posts', [UserController::class, 'postsCreatedByUser']);
+Route::get('/users/{user}/favorites', [UserController::class, 'postsLikedByUser']);
 
-Route::get('/users/{user}/posts', 'UserController@postsCreatedByUser');
-Route::get('/users/{user}/favorites', 'UserController@postsLikedByUser');
+Route::resource('ingredients', IngredientController::class);
+Route::resource('ingredients', IngredientController::class)
+    ->except(
+        [
+            'index', 'show'
+        ]
+    )
+    ->middleware('auth');
+Route::get('ingredients/{ingredient}/usage', [IngredientController::class, 'usage']);
 
-Route::resource('ingredients', 'IngredientController');
-Route::resource('ingredients', 'IngredientController')->except([
-    'index', 'show'
-])->middleware('auth');
-Route::get('ingredients/{ingredient}/usage', 'IngredientController@usage');
+Route::resource('dishes', DishController::class);
 
-Route::resource('dishes', 'DishController');
+Route::resource('posts', PostController::class);
+Route::resource('posts', PostController::class)
+    ->except(
+        [
+            'index', 'show', 'popularPosts'
+        ]
+    )->middleware('auth');
+Route::post('post/ing/save', [IngredientPostController::class, 'saveInSession'])
+    ->middleware('auth');
+Route::get('popular/posts', [PostController::class, 'popularPosts'])
+    ->name('posts.popular');
 
-Route::resource('posts', 'PostController');
-Route::resource('posts', 'PostController')->except([
-    'index', 'show', 'popularPosts'
-])->middleware('auth');
-Route::post('post/ing/save', 'IngredientPostController@saveInSession')->middleware('auth');
-Route::get('popular/posts', 'PostController@popularPosts')->name('posts.popular');
+Route::group(
+    ['prefix' => 'picture', 'middleware' => ['auth']],
+    function () {
+        Route::post('save', [PictureController::class, 'store']);
+        Route::post('update/{picture}', [PictureController::class, 'update']);
+        // Route::put('update/{picture}', [PictureController::class, 'update']); not allowed files
+        Route::delete('delete/{picture}', [PictureController::class, 'destroy']);
+    }
+);
 
-Route::group(['prefix' => 'picture', 'middleware' => ['auth']], function () {
-    Route::post('save', 'PictureController@store');
-    Route::post('update/{picture}', 'PictureController@update');
-    // Route::put('update/{picture}', 'PictureController@update'); not allowed files
-    Route::delete('delete/{picture}', 'PictureController@destroy');
-});
+Route::group(
+    ['prefix' => 'api/favorites'],
+    function () {
+        Route::post('{post}/action', [LikeController::class, 'reactToPost'])
+            ->middleware('auth');
+        Route::post('{post}/status', [LikeController::class, 'isReactedByUser'])
+            ->middleware('auth');
+        Route::post('{post}/count', [LikeController::class, 'likeCount']);
+    }
+);
 
-Route::group(['prefix' => 'api/favorites'], function () {
-    Route::post('{post}/action', 'LikeController@reactToPost')->middleware('auth');
-    Route::post('{post}/status', 'LikeController@isReactedByUser')->middleware('auth');
-    Route::post('{post}/count', 'LikeController@likeCount');
-});
-
-
-
-
-Route::redirect('/', 'home');
-
-// always in the end of list or Auth will break after page refresh
-Auth::routes();
+require __DIR__ . '/auth.php';
