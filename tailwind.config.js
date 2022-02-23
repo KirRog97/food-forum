02d3cdaf50727f4f9af2234cc601f14204e5417e
KirRog97 <KirRog97@yandex,ru>
2022-02-23 20:54:38 +0300

    Update Tailwind to 3 version

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,47 +1,51 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
-  purge: [
-    "./resources/**/*.blade.php",
-    "./resources/**/*.js",
-    "./resources/**/*.vue"
+  content: [
+    "./vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php",
+    "./storage/framework/views/*.php",
+    "./resources/views/**/*.blade.php",
+    "./resources/js/**/*.vue",
   ],
+
   theme: {
     fontFamily: {
       handwritten: ["Lobster", "cursive"],
       display: ["Oswald", "sans-serif"],
-      body: ["Oswald", "sans-serif"]
+      body: ["Oswald", "sans-serif"],
     },
     extend: {
       screens: {
         xs: "600px",
         sm: "768px",
         md: "992px",
-        lg: "1200px"
+        lg: "1200px",
       },
       colors: {
         primary: {
-          "50": "var(--color-primary50)",
-          "100": "var(--color-primary100)",
-          "200": "var(--color-primary200)",
-          "300": "var(--color-primary300)",
-          "400": "var(--color-primary400)",
-          "500": "var(--color-primary500)",
-          "600": "var(--color-primary600)",
-          "700": "var(--color-primary700)",
-          "800": "var(--color-primary800)",
-          "900": "var(--color-primary900)"
+          50: "var(--color-primary50)",
+          100: "var(--color-primary100)",
+          200: "var(--color-primary200)",
+          300: "var(--color-primary300)",
+          400: "var(--color-primary400)",
+          500: "var(--color-primary500)",
+          600: "var(--color-primary600)",
+          700: "var(--color-primary700)",
+          800: "var(--color-primary800)",
+          900: "var(--color-primary900)",
         },
         secondary: {
-          "50": "var(--color-secondary50)",
-          "100": "var(--color-secondary100)",
-          "200": "var(--color-secondary200)",
-          "300": "var(--color-secondary300)",
-          "400": "var(--color-secondary400)",
-          "500": "var(--color-secondary500)",
-          "600": "var(--color-secondary600)",
-          "700": "var(--color-secondary700)",
-          "800": "var(--color-secondary800)",
-          "900": "var(--color-secondary900)"
-        }
+          50: "var(--color-secondary50)",
+          100: "var(--color-secondary100)",
+          200: "var(--color-secondary200)",
+          300: "var(--color-secondary300)",
+          400: "var(--color-secondary400)",
+          500: "var(--color-secondary500)",
+          600: "var(--color-secondary600)",
+          700: "var(--color-secondary700)",
+          800: "var(--color-secondary800)",
+          900: "var(--color-secondary900)",
+        },
       },
       spacing: {
         "1/2": "50%",
@@ -69,16 +73,14 @@ module.exports = {
         "8/12": "66.666667%",
         "9/12": "75%",
         "10/12": "83.333333%",
-        "11/12": "91.666667%"
-      }
-    }
+        "11/12": "91.666667%",
+      },
+    },
   },
-  variants: {
-    animation: ["responsive", "motion-safe", "motion-reduce"]
-  },
+
   plugins: [
     require("@tailwindcss/forms"),
     require("@tailwindcss/typography"),
-    require("@tailwindcss/aspect-ratio")
-  ]
+    require("@tailwindcss/aspect-ratio"),
+  ],
 };
