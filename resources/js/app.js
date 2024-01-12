da1a131d5ea1c323b310a02585baa27e09180069
KirRog97 <kirrog97@yandex.ru>
2024-01-12 16:45:37 +0300

    Update JS config to Laravel 8

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,4 +1,5 @@
-require("./bootstrap");
+import "./bootstrap";
+import "../css/app.css";
 
 import { createApp, h } from "vue";
 import { createInertiaApp } from "@inertiajs/inertia-vue3";
@@ -7,6 +8,8 @@ import { InertiaProgress } from "@inertiajs/progress";
 import "../sass/element-ui/element-variables.scss";
 import ElementPlus from "element-plus";
 import ElementPlusRussianLocale from "element-plus/lib/locale/lang/ru";
+import { resolvePageComponent } from "laravel-vite-plugin/inertia-helpers";
+import { ZiggyVue } from "../../vendor/tightenco/ziggy/dist/vue.m";
 
 import { store } from "./store/index.js";
 
@@ -15,12 +18,17 @@ const appName =
 
 createInertiaApp({
   title: (title) => `${title} - ${appName}`,
-  resolve: (name) => require(`./Pages/${name}.vue`),
+  resolve: (name) =>
+    resolvePageComponent(
+      `./Pages/${name}.vue`,
+      import.meta.glob("./Pages/**/*.vue")
+    ),
   setup({ el, app, props, plugin }) {
     return createApp({ render: () => h(app, props) })
       .use(ElementPlus, {
         locale: ElementPlusRussianLocale,
       })
+      .use(ZiggyVue, Ziggy)
       .use(store)
       .use(plugin)
       .mixin({ methods: { route } })
