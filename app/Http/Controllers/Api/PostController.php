27b75afc21dee9e31765f5c5c202936afb52cc9b
KirRog97 <kirrog97@yandex.ru>
2024-02-11 14:54:28 +0300

    Fix Types of Api/PostController methods

--- app/Http/Controllers/Api/PostController.php
+++ app/Http/Controllers/Api/PostController.php
@@ -3,9 +3,9 @@
 namespace App\Http\Controllers\Api;
 
 use App\Models\Post;
-use Illuminate\Http\Response;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Collection;
 
 class PostController extends Controller
 {
@@ -13,20 +13,19 @@ class PostController extends Controller
      * Display a listing of the 10 most liked Posts.
      * 
      */
-    public function top(Post $post): Response
+    public function top(Post $post): Collection
     {
         return $post->getPopularPosts();
     }
 
-    public function new(Post $post): Response
+    public function new(Post $post): Collection
     {
         return Post::orderBy('created_at')
             ->take(10)
             ->get();
     }
 
-
-    public function editorChoise(Post $post): Response
+    public function editorChoise(Post $post): Collection
     {
         // Just a placeholder
         return Post::orderBy('user_id')
