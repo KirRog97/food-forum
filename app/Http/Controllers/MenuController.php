18bfe980bd55fff5745c973d65e95c45f9aed3d3
KirRog97 <KirRog97@yandex,ru>
2024-12-05 17:55:40 +0300

    Filled up the Menu controller methods

--- app/Http/Controllers/MenuController.php
+++ app/Http/Controllers/MenuController.php
@@ -3,83 +3,76 @@
 namespace App\Http\Controllers;
 
 use App\Models\Menu;
+use App\Models\Post;
+use Inertia\Inertia;
+use Inertia\Response;
+use App\Models\Picture;
 use Illuminate\Http\Request;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Support\Facades\Redirect;
 
 class MenuController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
+    public function create() : Response
     {
-        //
+        return Inertia::render('Menus/Create');
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
+    public function store(Request $request, Menu $menu) : RedirectResponse
     {
-        //
+        $picture = Picture::create([
+            'path' => '/storage/' . $request->file->store('menus', 'public'),
+            'mime' => $request->file->getMimeType(),
+            'size' => $request->file->getSize()
+        ]);
+
+        Menu::create([
+            'name' => $request->name,
+            'picture_id' => $picture->id,
+            'description' => $request->description
+        ]);
+
+        return Redirect::route('home');
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
+    public function show(Menu $menu) : Response
     {
-        //
+        return Inertia::render(
+            'Menus/Show',
+            [
+                'menu' => $menu->load(
+                    'picture:id,path'
+                ),
+                'posts' => Post::where('menu_id', $menu->id)
+                    ->paginate(12)
+            ]
+        );
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Models\Menu  $menu
-     * @return \Illuminate\Http\Response
-     */
-    public function show(Menu $menu)
+    public function edit(Menu $menu) : Response
     {
-        //
+        return Inertia::render(
+            'Menus/Edit',
+            [
+                'menu' => $menu
+            ]
+        );
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Models\Menu  $menu
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Menu $menu)
+    public function update(Request $request, Menu $menu) : RedirectResponse
     {
-        //
+        $menu->fill($request->validate([
+            'name' => 'required',
+            'description' => 'required',
+        ]));
+        $menu->save();
+
+        return Redirect::route('menus.show', $menu);
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\Menu  $menu
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, Menu $menu)
+    public function destroy(Menu $menu) : RedirectResponse
     {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Models\Menu  $menu
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(Menu $menu)
-    {
-        //
+        $menu->delete();
+        return Redirect::route('home');
     }
 }
