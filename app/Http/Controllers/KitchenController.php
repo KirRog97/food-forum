34a55db4ad128f9850654d4352e20f116b189e63
KirRog97 <KirRog97@yandex,ru>
2024-12-05 17:39:21 +0300

    Filled up the Kitchen controller methods

--- app/Http/Controllers/KitchenController.php
+++ app/Http/Controllers/KitchenController.php
@@ -2,84 +2,79 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Post;
+use Inertia\Inertia;
+use Inertia\Response;
 use App\Models\Kitchen;
+use App\Models\Picture;
 use Illuminate\Http\Request;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Support\Facades\Redirect;
 
 class KitchenController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
+    public function create() : Response
     {
-        //
+        return Inertia::render('Kitchens/Create');
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
+    public function store(Request $request) : RedirectResponse
     {
-        //
+        $picture = Picture::create([
+            'path' => '/storage/' . $request->file->store('kitchens', 'public'),
+            'mime' => $request->file->getMimeType(),
+            'size' => $request->file->getSize()
+        ]);
+
+        Kitchen::create([
+            'name' => $request->name,
+            'picture_id' => $picture->id,
+            'description' => $request->description
+        ]);
+
+        return Redirect::route('home');
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
+    public function show(Kitchen $kitchen) : Response
     {
-        //
+        return Inertia::render(
+            'Kitchens/Show',
+            [
+                'kitchen' => $kitchen,
+                'posts' => Post::where('kitchen_id', $kitchen->id)
+                    ->paginate(12)
+            ]
+        );
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Models\Kitchen  $kitchen
-     * @return \Illuminate\Http\Response
-     */
-    public function show(Kitchen $kitchen)
-    {
-        //
-    }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Models\Kitchen  $kitchen
-     * @return \Illuminate\Http\Response
-     */
     public function edit(Kitchen $kitchen)
     {
-        //
+        return Inertia::render(
+            'Kitchens/Edit',
+            [
+                'kitchen' => $kitchen
+            ]
+        );
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\Kitchen  $kitchen
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, Kitchen $kitchen)
+
+    public function update(Request $request, Kitchen $kitchen) : RedirectResponse
     {
-        //
+        $kitchen->fill($request->validate([
+            'name' => 'required',
+            'description' => 'required',
+        ]));
+
+        $kitchen->save();
+
+        return Redirect::route('kitchens.show', $kitchen);
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Models\Kitchen  $kitchen
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(Kitchen $kitchen)
+
+    public function destroy(Kitchen $kitchen) : RedirectResponse
     {
-        //
+        $kitchen->delete();
+        return Redirect::route('home');
     }
 }
