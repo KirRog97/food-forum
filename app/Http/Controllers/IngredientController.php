1ffdb72f9c570b4e3f08edefd0e26ccc9b264b46
KirRog97 <KirRog97@yandex,ru>
2024-12-06 11:15:23 +0300

    Rework and fill up Ingredient controller

--- app/Http/Controllers/IngredientController.php
+++ app/Http/Controllers/IngredientController.php
@@ -4,18 +4,15 @@ namespace App\Http\Controllers;
 
 use Inertia\Inertia;
 use Inertia\Response;
+use App\Models\Picture;
 use App\Models\Ingredient;
+use Illuminate\Http\Request;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Support\Facades\Redirect;
 
 class IngredientController extends Controller
 {
-
-    /**
-     * Display a listing of all ingredients
-     *
-     * @param  \App\Models\Ingredient  $ingredient
-     * @return \Illuminate\Http\Response
-     */
-    public function index(Ingredient $ingredients): Response
+    public function index(Ingredient $ingredients) : Response
     {
         $ingredients = Ingredient::orderBy('name', 'asc')->with('picture:id,path');
 
@@ -33,7 +30,7 @@ class IngredientController extends Controller
      * @param  \App\Models\Ingredient  $ingredient
      * @return \Illuminate\Http\Response
      */
-    public function usage(Ingredient $ingredient): Response
+    public function usage(Ingredient $ingredient) : Response
     {
         return Inertia::render(
             'Ingredients/IngredientUsage',
@@ -44,13 +41,24 @@ class IngredientController extends Controller
         );
     }
 
-    /**
-     * Display the particular ingredient.
-     *
-     * @param  \App\Models\Ingredient  $ingredient
-     * @return \Illuminate\Http\Response
-     */
-    public function show(Ingredient $ingredient): Response
+    public function store(Request $request) : RedirectResponse
+    {
+        $picture = Picture::create([
+            'path' => '/storage/' . $request->file->store('categories', 'public'),
+            'mime' => $request->file->getMimeType(),
+            'size' => $request->file->getSize()
+        ]);
+
+        Ingredient::create([
+            'name' => $request->name,
+            'picture_id' => $picture->id,
+            'description' => $request->description
+        ]);
+
+        return Redirect::route('home');
+    }
+
+    public function show(Ingredient $ingredient) : Response
     {
         return Inertia::render(
             'Ingredients/Show',
@@ -58,7 +66,43 @@ class IngredientController extends Controller
                 'ingredient' => $ingredient->load(
                     'picture:id,path'
                 ),
+                'posts' => $ingredient->posts()->paginate(10)
             ]
         );
+    }
+
+
+    public function create() : Response
+    {
+        return Inertia::render('Ingredients/Create');
+    }
+
+    public function edit(Ingredient $ingredient) : Response
+    {
+        return Inertia::render(
+            'Ingredients/Edit',
+            [
+                'ingredient' => $ingredient->loadMissing('picture', 'posts')
+            ]
+        );
+    }
+
+    public function update(Request $request, Ingredient $ingredient) : RedirectResponse
+    {
+        $ingredient->fill($request->validate([
+            'name' => 'required',
+            'description' => 'required',
+        ]));
+
+        $ingredient->save();
+
+        return Redirect::route('ingredients.show', $ingredient);
+    }
+
+    public function destroy(Ingredient $ingredient) : RedirectResponse
+    {
+        $ingredient->delete();
+
+        return Redirect::route('ingredients.index');
     }
 }
