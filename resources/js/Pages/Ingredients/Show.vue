8d9e643d4eb18b7291cce486e2e7a864c6f567ad
KirRog97 <KirRog97@yandex.ru>
2026-02-05 14:11:07 +0300

    Switch Ingredient Show to more universal layout

--- resources/js/Pages/Ingredients/Show.vue
+++ resources/js/Pages/Ingredients/Show.vue
@@ -1,9 +1,15 @@
 <script setup>
 import { Head } from "@inertiajs/vue3";
 import AppLayout from "@/Layouts/AppLayout.vue";
+import GenericShowLayout from "@/Layouts/GenericShowLayout.vue";
+import RecipeLayout from "@/Layouts/RecipeLayout.vue";
 
 defineProps({
   ingredient: {
+    type: Object,
+    default: {},
+  },
+  recipes: {
     type: Object,
     default: {},
   },
@@ -12,45 +18,15 @@ defineProps({
 
 <template>
   <AppLayout>
-    <Head :title="`Ингредиент ${ingredient.name}`" />
-
-    <div class="flex flex-col mx-auto">
-      <div class="relative w-full bg-secondary-100">
-        <div class="relative pb-2/3">
-          <img
-            class="absolute h-full w-full object-cover"
-            :src="ingredient.picture.path"
-            :alt="`Ingredient ${ingredient.name}
-          image`"
-          />
-
-          <!-- {{-- Placeholder --}} -->
-
-          <!-- <img
-          class="absolute h-full w-full object-cover"
-          src="/images/login_background.jpg"
-          alt="Recipe image"
-        /> -->
-        </div>
-      </div>
-
-      <div
-        class="flex flex-auto flex-wrap justify-center bg-secondary-100 px-6 py-8 mx-auto space-y-6 sm:space-y-8"
-      >
-        <div class="flex justify-center flex-wrap">
-          <span
-            class="text-2xl sm:text-4xl text-primary-600 text-center leading-tight font-handwritten mb-1 sm:mb-2"
-          >
-            {{ ingredient.name }}
-          </span>
-
-          <p
-            class="w-full text-base sm:text-lg text-secondary-800 leading-snug sm:leading-normal"
-          >
-            {{ ingredient.description }}
-          </p>
-        </div>
-      </div>
-    </div>
+    <Head :title="`Ингредиент &quot;${ingredient.name}&quot;`" />
+    <GenericShowLayout :entity="ingredient">
+      <template #recipes>
+        <RecipeLayout
+          :headingText="`Рецепты включающие в состав &quot;${ingredient.name}&quot;`"
+          :paginationLinks="recipes.links"
+          :recipes="recipes.data"
+        />
+      </template>
+    </GenericShowLayout>
   </AppLayout>
 </template>
